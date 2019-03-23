ad4b08dedc60f253c1ebe6c071d7ea9d59e6e5cb
Alex Ngun <ukmerryxd@gmail.com>
2019-03-22 18:23:31 -0700

    connect all the buttons with signals and slots

--- TicTacToe/tictactoe.cpp
+++ TicTacToe/tictactoe.cpp
@@ -1,38 +1,180 @@
 #include "tictactoe.h"
 #include "ui_tictactoe.h"
-#include <array>
+#include <QColor>
 
 using namespace std;
 
 TicTacToe::TicTacToe(QWidget *parent) :
     QMainWindow(parent),
-    ui(new Ui::TicTacToe)
+    ui(new Ui::TicTacToe), flag(0)
 {
     ui->setupUi(this);
-
-
-
-    QObject::connect(ui->toolButton_11, SIGNAL(clicked()),
-                       this, SLOT(grid[0][0] = value));
-    QObject::connect(ui->toolButton_12, SIGNAL(clicked()),
-                       this, SLOT(grid[0][1] = value));
-    QObject::connect(ui->toolButton_13, SIGNAL(clicked()),
-                       this, SLOT(grid[0][2] = value));
-    QObject::connect(ui->toolButton_21, SIGNAL(clicked()),
-                       this, SLOT(grid[1][0] = value));
-    QObject::connect(ui->toolButton_22, SIGNAL(clicked()),
-                       this, SLOT(grid[1][1] = value));
-    QObject::connect(ui->toolButton_23, SIGNAL(clicked()),
-                       this, SLOT(grid[1][2] = value));
-    QObject::connect(ui->toolButton_31, SIGNAL(clicked()),
-                       this, SLOT(grid[2][0] = value));
-    QObject::connect(ui->toolButton_32, SIGNAL(clicked()),
-                       this, SLOT(grid[2][1] = value));
-    QObject::connect(ui->toolButton_33, SIGNAL(clicked()),
-                       this, SLOT(grid[2][2] = value));
 }
+
 
 TicTacToe::~TicTacToe()
 {
     delete ui;
+}
+
+void TicTacToe::on_toolButton_11_clicked()
+{
+    grid[0][0] = flag;
+
+    if (flag == 1)
+    {
+        ui->toolButton_11->setPalette(Qt::red);
+        ui->toolButton_11->setAutoFillBackground(true);
+        flag = 0;
+    }
+    else if(flag == 0)
+    {
+        ui->toolButton_11->setPalette(Qt::blue);
+        ui->toolButton_11->setAutoFillBackground(true);
+        flag = 1;
+    }
+}
+
+void TicTacToe::on_toolButton_12_clicked()
+{
+    grid[0][1] = flag;
+
+    if (flag == 1)
+    {
+        ui->toolButton_12->setPalette(Qt::red);
+        ui->toolButton_12->setAutoFillBackground(true);
+        flag = 0;
+    }
+    else if(flag == 0)
+    {
+        ui->toolButton_12->setPalette(Qt::blue);
+        ui->toolButton_12->setAutoFillBackground(true);
+        flag = 1;
+    }
+}
+
+void TicTacToe::on_toolButton_13_clicked()
+{
+    grid[0][2] = flag;
+
+    if (flag == 1)
+    {
+        ui->toolButton_13->setPalette(Qt::red);
+        ui->toolButton_13->setAutoFillBackground(true);
+        flag = 0;
+    }
+    else if(flag == 0)
+    {
+        ui->toolButton_13->setPalette(Qt::blue);
+        ui->toolButton_13->setAutoFillBackground(true);
+        flag = 1;
+    }
+}
+
+void TicTacToe::on_toolButton_21_clicked()
+{
+    grid[1][0] = flag;
+
+    if (flag == 1)
+    {
+        ui->toolButton_21->setPalette(Qt::red);
+        ui->toolButton_21->setAutoFillBackground(true);
+        flag = 0;
+    }
+    else if(flag == 0)
+    {
+        ui->toolButton_21->setPalette(Qt::blue);
+        ui->toolButton_21->setAutoFillBackground(true);
+        flag = 1;
+    }
+}
+
+void TicTacToe::on_toolButton_22_clicked()
+{
+    grid[1][1] = flag;
+
+    if (flag == 1)
+    {
+        ui->toolButton_22->setPalette(Qt::red);
+        ui->toolButton_22->setAutoFillBackground(true);
+        flag = 0;
+    }
+    else if(flag == 0)
+    {
+        ui->toolButton_22->setPalette(Qt::blue);
+        ui->toolButton_22->setAutoFillBackground(true);
+        flag = 1;
+    }
+}
+
+void TicTacToe::on_toolButton_23_clicked()
+{
+    grid[1][2] = flag;
+
+    if (flag == 1)
+    {
+        ui->toolButton_23->setPalette(Qt::red);
+        ui->toolButton_23->setAutoFillBackground(true);
+        flag = 0;
+    }
+    else if(flag == 0)
+    {
+        ui->toolButton_23->setPalette(Qt::blue);
+        ui->toolButton_23->setAutoFillBackground(true);
+        flag = 1;
+    }
+}
+
+void TicTacToe::on_toolButton_31_clicked()
+{
+    grid[2][0] = flag;
+
+    if (flag == 1)
+    {
+        ui->toolButton_31->setPalette(Qt::red);
+        ui->toolButton_31->setAutoFillBackground(true);
+        flag = 0;
+    }
+    else if(flag == 0)
+    {
+        ui->toolButton_31->setPalette(Qt::blue);
+        ui->toolButton_31->setAutoFillBackground(true);
+        flag = 1;
+    }
+}
+
+void TicTacToe::on_toolButton_32_clicked()
+{
+    grid[2][1] = flag;
+
+    if (flag == 1)
+    {
+        ui->toolButton_32->setPalette(Qt::red);
+        ui->toolButton_32->setAutoFillBackground(true);
+        flag = 0;
+    }
+    else if(flag == 0)
+    {
+        ui->toolButton_32->setPalette(Qt::blue);
+        ui->toolButton_32->setAutoFillBackground(true);
+        flag = 1;
+    }
+}
+
+void TicTacToe::on_toolButton_33_clicked()
+{
+    grid[2][2] = flag;
+
+    if (flag == 1)
+    {
+        ui->toolButton_33->setPalette(Qt::red);
+        ui->toolButton_33->setAutoFillBackground(true);
+        flag = 0;
+    }
+    else if(flag == 0)
+    {
+        ui->toolButton_33->setPalette(Qt::blue);
+        ui->toolButton_33->setAutoFillBackground(true);
+        flag = 1;
+    }
 }
--- TicTacToe/tictactoe.h
+++ TicTacToe/tictactoe.h
@@ -20,9 +20,30 @@ public:
     explicit TicTacToe(QWidget *parent = nullptr);
     ~TicTacToe();
 
+private slots:
+    void on_toolButton_11_clicked();
+
+    void on_toolButton_12_clicked();
+
+    void on_toolButton_13_clicked();
+
+    void on_toolButton_21_clicked();
+
+    void on_toolButton_22_clicked();
+
+    void on_toolButton_23_clicked();
+
+    void on_toolButton_31_clicked();
+
+    void on_toolButton_32_clicked();
+
+    void on_toolButton_33_clicked();
+
 private:
     Ui::TicTacToe *ui;
-    array <array<int, dem>, dem> grid;
+
+    bool flag;
+    array <array<bool, dem>, dem> grid;
 };
 
 #endif // TICTACTOE_H
